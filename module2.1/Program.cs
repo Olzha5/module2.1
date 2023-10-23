5da6c838c42c93e0eb23120212be329a23c61389
Olzhas <olzhasabukhan2003@gmail.com>
2023-10-23 09:57:26 +0600

    Добавьте файлы проекта.

---
+++ module2.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CountSpaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите символы. Для завершения ввода введите точку.");
+
+            int spaceCount = 0;
+            char ch;
+
+            do
+            {
+                ch = (char)Console.Read();
+
+                if (ch == ' ')
+                {
+                    spaceCount++;
+                }
+
+            } while (ch != '.');
+
+            Console.WriteLine($"Количество введенных пробелов: {spaceCount}");
+        }
+    }
+}
+
